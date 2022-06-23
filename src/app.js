8e7acc4607d03f5e0e24450bc22294f42a83b48d
Andreas Ntokas <ntokas.an@gmail.com>
2022-06-23 19:42:37 +0300

    File separation, WIP

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import sha1 from 'js-sha1';
+import { fromHex } from './fromHex';
 
 const boardElem = document.getElementById('board');
 const inputElem = document.getElementById('input');
@@ -8,49 +9,6 @@ let state = {
   board: Array(25).fill(''),
   color: '',
 };
-
-const HEX_STRINGS = '0123456789abcdef';
-
-const MAP_HEX = {
-  0: 0,
-  1: 1,
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-  a: 10,
-  b: 11,
-  c: 12,
-  d: 13,
-  e: 14,
-  f: 15,
-  A: 10,
-  B: 11,
-  C: 12,
-  D: 13,
-  E: 14,
-  F: 15,
-};
-
-// https://github.com/nodejs/node/blob/v14.18.1/src/string_bytes.cc#L246-L261
-function fromHex(hexString) {
-  const bytes = new Uint8Array(Math.floor((hexString || '').length / 2));
-  let i;
-  for (i = 0; i < bytes.length; i++) {
-    const a = MAP_HEX[hexString[i * 2]];
-    const b = MAP_HEX[hexString[i * 2 + 1]];
-    if (a === undefined || b === undefined) {
-      break;
-    }
-    bytes[i] = (a << 4) | b;
-  }
-
-  return i === bytes.length ? bytes : bytes.slice(0, i);
-}
 
 function clear(element) {
   element.innerHTML = '';
@@ -105,8 +63,6 @@ function rgbToHex(r, g, b) {
     componentToHex(state.board[0]) +
     componentToHex(state.board[1]) +
     componentToHex(state.board[2]);
-
-  console.log(state.color);
 }
 
 inputElem.addEventListener('input', (e) => {
